eed86e4cbc035e7447af27d214b194ee88d15002
Nikita <nik007rev@gmail.com>
2023-11-19 15:31:20 +0300

    готово

--- Bundle. Sandbox. FileManager/SceneDelegate.swift
+++ Bundle. Sandbox. FileManager/SceneDelegate.swift
@@ -13,12 +13,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+           guard let windowScene = (scene as? UIWindowScene) else { return }
+           
+           // Создаем UIWindow и привязываем его к UIWindowScene
+           let window = UIWindow(windowScene: windowScene)
+           
+           // Создаем ViewController и навигационный контроллер
+           let viewController = ViewController()
+           let navigationController = UINavigationController(rootViewController: viewController)
+           
+           // Устанавливаем navigationController как rootViewController
+           window.rootViewController = navigationController
+           
+           // Устанавливаем окно приложения
+           self.window = window
+           window.makeKeyAndVisible()
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
--- Bundle. Sandbox. FileManager/ViewController.swift
+++ Bundle. Sandbox. FileManager/ViewController.swift
@@ -1,19 +1,109 @@
-//
-//  ViewController.swift
-//  Bundle. Sandbox. FileManager
-//
-//  Created by Никита  on 19.11.2023.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+    var contents: [URL] = []
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = self
+        tableView.delegate = self
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        loadContents()
     }
-
-
+    
+    func setupUI() {
+        title = "Documents"
+        view.addSubview(tableView)
+        tableView.frame = view.bounds
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Добавить фотографию", style: .plain, target: self, action: #selector(addPhoto))
+    }
+    
+    @objc func loadContents() {
+        do {
+            contents = try FileManager.default.contentsOfDirectory(at: documentsDirectory, includingPropertiesForKeys: nil, options: [])
+            tableView.reloadData()
+        } catch {
+            showAlert(title: "Ошибка", message: "Не удалось загрузить содержимое директории.")
+            print("ошибка: \(error.localizedDescription)")
+        }
+    }
+    
+    @objc func addPhoto() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+  
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            let imageURL = documentsDirectory.appendingPathComponent(UUID().uuidString).appendingPathExtension("png")
+            if let imageData = image.pngData() {
+                do {
+                    try imageData.write(to: imageURL)
+                    contents.append(imageURL)
+                    tableView.reloadData()
+                } catch {
+                    showAlert(title: "Ошибка", message: "Не удалось сохранить изображение.")
+                    print("Error saving image: \(error.localizedDescription)")
+                }
+            }
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+   
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") ?? UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        let fileURL = contents[indexPath.row]
+        cell.textLabel?.text = fileURL.lastPathComponent
+        return cell
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        _ = contents[indexPath.row]
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: "Удалить") { [weak self] (_, _, completionHandler) in
+            self?.deleteFile(at: indexPath)
+            completionHandler(true)
+        }
+        return UISwipeActionsConfiguration(actions: [deleteAction])
+    }
+    
+    func deleteFile(at indexPath: IndexPath) {
+        let fileURL = contents[indexPath.row]
+        do {
+            try FileManager.default.removeItem(at: fileURL)
+            contents.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        } catch {
+            showAlert(title: "Ошибка", message: "Не удалось удалить файл.")
+            print("Error deleting file: \(error.localizedDescription)")
+        }
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
 }
-
